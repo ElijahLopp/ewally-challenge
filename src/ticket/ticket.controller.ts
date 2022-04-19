c1296bc7ca880ba71e62232724906ba4468bbed5
elias.lopes <elias.lopes@nexodata.com.br>
2022-04-19 18:03:36 -0300

    Add test e2e

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { ValidationCode } from '../pipes/validation.pipe';
 import { TicketResponse } from './interfaces/ticket.interface';
@@ -7,10 +13,20 @@ import { TicketResponse } from './interfaces/ticket.interface';
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
+  @Get('status')
+  getStatus(): boolean {
+    return true;
+  }
+
   @Get(':code')
-  async getInformations(
-    @Param('code', ValidationCode) code: string,
-  ): Promise<TicketResponse> {
+  getInformations(@Param('code', ValidationCode) code: string): TicketResponse {
+    try {
+    } catch (error) {
+      throw new HttpException(
+        'contact our team for more information',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
     return this.ticketService.getInformations(code);
   }
 }
--- test/ticket.e2e-spec.ts
+++ test/ticket.e2e-spec.ts
@@ -1,8 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
 import { TicketModule } from '../src/ticket/ticket.module';
+
+const RESPONSE_MOCK = () => ({
+  goodResponse: {
+    barCode: '21299758700000020000001121100012100447561740',
+    amount: '20.00',
+    expirationDate: '2018-07-16',
+  },
+  badResponseNumOnly: {
+    statusCode: HttpStatus.BAD_REQUEST,
+    message: 'the line is typed is not within the expected pattern',
+    error: 'Bad Request',
+  },
+  badReponseChar: {
+    statusCode: HttpStatus.BAD_REQUEST,
+    message: 'only numbers on the line',
+    error: 'Bad Request',
+  },
+});
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,7 +33,31 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/api/boleto').expect(200);
+  it('/boleto/:code (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/boleto/21290001192110001210904475617405975870000002000')
+      .expect(HttpStatus.OK)
+      .expect(RESPONSE_MOCK().goodResponse);
+  });
+
+  it('/boleto/:code (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/boleto/21290001192110001210904475617405975870000002000e')
+      .expect(HttpStatus.BAD_REQUEST)
+      .expect(RESPONSE_MOCK().badReponseChar);
+  });
+
+  it('/boleto/:code (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/boleto/2129000119211000121090447561740597587000000200')
+      .expect(HttpStatus.BAD_REQUEST)
+      .expect(RESPONSE_MOCK().badResponseNumOnly);
+  });
+
+  it('/boleto/:code (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/boleto/2129000119211000121090447561740597587000000200778')
+      .expect(HttpStatus.BAD_REQUEST)
+      .expect(RESPONSE_MOCK().badResponseNumOnly);
   });
 });
